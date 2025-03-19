77b72aa0312db469c6d4a584e19ea985b344750f
BernardKazazic <bernard.kazazic@gmail.com>
2025-03-19 23:24:59 +0100

    init commit

---
+++ src/main/java/fer/solar/solar_api_gateway/SolarApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package fer.solar.solar_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fer/solar/solar_api_gateway/SolarApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package fer.solar.solar_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
