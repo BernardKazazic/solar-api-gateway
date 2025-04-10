38b6f7e7350ecb430062b88fd7d9790ee11f0757
BernardKazazic <92548057+BernardKazazic@users.noreply.github.com>
2025-04-10 21:20:32 +0200

    SOL-11 Add user creation

---
+++ src/main/java/fer/solar/solar_api_gateway/config/ServiceUrisConfig.java
@@ -0,0 +1,18 @@
+package fer.solar.solar_api_gateway.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Configuration
+@ConfigurationProperties(prefix = "service.uri")
+@Getter
+@Setter
+public class ServiceUrisConfig {
+
+    private String userManagement;
+    private String mockFlask;
+
+} 
--- src/main/java/fer/solar/solar_api_gateway/routing/RouteConfig.java
+++ src/main/java/fer/solar/solar_api_gateway/routing/RouteConfig.java
@@ -1,23 +1,59 @@
 package fer.solar.solar_api_gateway.routing;
 
+import fer.solar.solar_api_gateway.config.ServiceUrisConfig;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class RouteConfig {
+
+    private final ServiceUrisConfig serviceUrisConfig;
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-            // Route for the forecast service
-            .route("forecast-service", r -> r
-                .path("/api/forecast/**")
+            // Route for the user management service
+            .route("user-management-service", r -> r
+                .path("/users/**")
                 .filters(f -> f
-                    .rewritePath("/api/forecast/(?<segment>.*)", "/${segment}")
+                    .rewritePath("/users(?<segment>/?.*)", "/api/v1/users${segment}")
                     .addRequestHeader("X-Gateway-Request", "true"))
-                .uri("lb://forecast-service"))
+                .uri(serviceUrisConfig.getUserManagement()))
+                
+            // Routes for the mock Flask service (running on port 5000)
+            .route("mock-upload", r -> r
+                .path("/upload/**")
+                .filters(f -> f.addRequestHeader("X-Gateway-Request", "true"))
+                .uri(serviceUrisConfig.getMockFlask()))
+            .route("mock-power-plants", r -> r
+                .path("/power_plants/**")
+                .filters(f -> f.addRequestHeader("X-Gateway-Request", "true"))
+                .uri(serviceUrisConfig.getMockFlask()))
+            .route("mock-models", r -> r
+                .path("/models/**")
+                .filters(f -> f.addRequestHeader("X-Gateway-Request", "true"))
+                .uri(serviceUrisConfig.getMockFlask()))
+             .route("mock-dashboard", r -> r
+                .path("/dashboard/**")
+                .filters(f -> f.addRequestHeader("X-Gateway-Request", "true"))
+                .uri(serviceUrisConfig.getMockFlask()))
+            .route("mock-events", r -> r
+                .path("/events/**")
+                .filters(f -> f.addRequestHeader("X-Gateway-Request", "true"))
+                .uri(serviceUrisConfig.getMockFlask()))
+            .route("mock-forecasts", r -> r
+                .path("/forecasts/**")
+                .filters(f -> f.addRequestHeader("X-Gateway-Request", "true"))
+                .uri(serviceUrisConfig.getMockFlask()))
+            .route("mock-metrics", r -> r
+                .path("/metrics/**")
+                .filters(f -> f.addRequestHeader("X-Gateway-Request", "true"))
+                .uri(serviceUrisConfig.getMockFlask()))
                 
             // Add more routes for other microservices here
             // Example:
