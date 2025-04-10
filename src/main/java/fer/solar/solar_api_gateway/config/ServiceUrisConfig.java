36c1f07b38b943a3b73b34e0c6ba79536bbd5b86
BernardKazazic <bernard.kazazic@gmail.com>
2025-04-10 21:19:52 +0200

    pr fix 2

--- src/main/java/fer/solar/solar_api_gateway/config/ServiceUrisConfig.java
+++ src/main/java/fer/solar/solar_api_gateway/config/ServiceUrisConfig.java
@@ -3,28 +3,16 @@ package fer.solar.solar_api_gateway.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Configuration
 @ConfigurationProperties(prefix = "service.uri")
+@Getter
+@Setter
 public class ServiceUrisConfig {
 
     private String userManagement;
     private String mockFlask;
 
-    // Getters and Setters
-
-    public String getUserManagement() {
-        return userManagement;
-    }
-
-    public void setUserManagement(String userManagement) {
-        this.userManagement = userManagement;
-    }
-
-    public String getMockFlask() {
-        return mockFlask;
-    }
-
-    public void setMockFlask(String mockFlask) {
-        this.mockFlask = mockFlask;
-    }
 } 
